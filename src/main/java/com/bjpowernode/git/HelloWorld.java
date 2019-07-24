cd7ff95a80146a1f30e1c5c37a784b70b7b056c2
Lei <15578870295@163.com>
2019-07-24 09:46:26 +0800

    test1

---
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.git;
+
+/**
+ * ClassName:HelloWorld
+ * Package:com.bjpowernode.git
+ * Desc:
+ * Date:2019/7/24 9:18
+ * Author:Lei
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello lei");
+    }
+}
